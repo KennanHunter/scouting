8c0da0e653586d7fa4e528bb868dbfbed79e3091
liujip0 <136777216+liujip0@users.noreply.github.com>
2023-12-28 11:02:22 -0500

    Added spacers as a form element

--- app/app/src/main/java/com/example/isaproject/FormViewModel.kt
+++ app/app/src/main/java/com/example/isaproject/FormViewModel.kt
@@ -69,6 +69,12 @@ private val formJSON = """
             {
                 "type": "space",
                 "name": "sdlk"
+            },
+            {
+                "type": "number",
+                "name": "abcde",
+                "initialValue": "0",
+                "label": "Another number"
             }
         ]
     }
