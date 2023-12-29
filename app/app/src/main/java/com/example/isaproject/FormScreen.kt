ba87abe8b5267a6a83d44edd5b9313d3eef1b5d4
liujip0 <136777216+liujip0@users.noreply.github.com>
2023-12-29 00:23:05 -0500

    Started on screen for selecting devices

--- app/app/src/main/java/com/example/isaproject/FormScreen.kt
+++ app/app/src/main/java/com/example/isaproject/FormScreen.kt
@@ -7,13 +7,19 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedButton
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.isaproject.ui.theme.ISAProjectTheme
+import kotlinx.coroutines.flow.callbackFlow
 
 @Composable
 fun FormScreen(
@@ -23,11 +29,18 @@ fun FormScreen(
     onPreviousButtonClicked: () -> Unit,
     modifier: Modifier = Modifier
 ) {
-    Column(
-        modifier = modifier.padding(all = dimensionResource(R.dimen.margin))
-    ) {
+    Column() {
+        Text(
+            text = page.name,
+            style = MaterialTheme.typography.headlineMedium,
+            modifier = Modifier
+                .padding(all = dimensionResource(R.dimen.margin))
+                .align(Alignment.CenterHorizontally)
+        )
         LazyColumn(
-            modifier = Modifier.weight(1f)
+            modifier = Modifier
+                .weight(1f)
+                .padding(all = dimensionResource(R.dimen.margin))
         ) {
             items(
                 items = page.page,
@@ -43,24 +56,26 @@ fun FormScreen(
                 )
             }
         }
-        Row(
-            modifier = modifier
+        Surface(
+            color = MaterialTheme.colorScheme.surfaceContainer
         ) {
-            OutlinedButton(
-                onClick = onPreviousButtonClicked,
-                modifier = Modifier
-                    .padding(horizontal = dimensionResource(R.dimen.margin))
-                    .weight(1f)
-            ) {
-                Text(stringResource(R.string.previous))
-            }
-            Button(
-                onClick = onNextButtonClicked,
-                modifier = Modifier
-                    .padding(horizontal = dimensionResource(R.dimen.margin))
-                    .weight(1f)
-            ) {
-                Text(stringResource(R.string.next))
+            Row {
+                OutlinedButton(
+                    onClick = onPreviousButtonClicked,
+                    modifier = Modifier
+                        .padding(all = dimensionResource(R.dimen.margin))
+                        .weight(1f)
+                ) {
+                    Text(stringResource(R.string.previous))
+                }
+                Button(
+                    onClick = onNextButtonClicked,
+                    modifier = Modifier
+                        .padding(all = dimensionResource(R.dimen.margin))
+                        .weight(1f)
+                ) {
+                    Text(stringResource(R.string.next))
+                }
             }
         }
     }
