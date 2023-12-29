1ec623ace61bdd8eb5bdbd00320feb114d16c0f5
liujip0 <136777216+liujip0@users.noreply.github.com>
2023-12-28 20:42:48 -0500

    Separated the JSON into pages

--- app/app/src/main/java/com/example/isaproject/FormScreen.kt
+++ app/app/src/main/java/com/example/isaproject/FormScreen.kt
@@ -9,13 +9,14 @@ import androidx.lifecycle.viewmodel.compose.viewModel
 @Composable
 fun FormScreen(
     formViewModel: FormViewModel,
+    page: FormPage,
     modifier: Modifier = Modifier
 ) {
     LazyColumn(
         modifier = modifier
     ) {
         items(
-            items = formViewModel.form,
+            items = page.page,
             key = { item -> item.name }
         ) {item ->
             formItem(
@@ -24,7 +25,7 @@ fun FormScreen(
                 placeholder = item.placeholder,
                 options = item.options,
                 value = item.value,
-                onValueChange = { value -> formViewModel.changeValue(item, value) }
+                onValueChange = { value -> formViewModel.changeValue(page, item, value) }
             )
         }
     }
--- app/app/src/main/java/com/example/isaproject/FormViewModel.kt
+++ app/app/src/main/java/com/example/isaproject/FormViewModel.kt
@@ -6,12 +6,14 @@ import kotlinx.serialization.json.Json
 
 class FormViewModel : ViewModel() {
     private val _form = getForm().toMutableStateList()
-    val form: List<FormElement>
+    val form: List<FormPage>
         get() = _form
 
-    fun changeValue(item: FormElement, value: String) {
-        _form.find {it.name == item.name}?.let { i ->
-            i.value = value
+    fun changeValue(page: FormPage, item: FormElement, value: String) {
+        _form.find {it.name == page.name}?.let { i ->
+            i.page.find {it.name == item.name}?. let { j ->
+                j.value = value
+            }
         }
     }
 }
@@ -20,66 +22,76 @@ private val formJSON = """
     {
         "form": [
             {
-                "type": "text",
-                "label": "bar",
-                "name": "text",
-                "placeholder": "text"
-            },
-            {
-                "type": "number",
-                "label": "foo",
-                "name": "number",
-                "placeholder": "number",
-                "initialValue": "0"
-            },
-            {
-                "type": "divider",
-                "name": "divider"
-            },
-            {
-                "type": "radio",
-                "label": "lorem ipsum sit dolor amet",
-                "name": "radio",
-                "options": [
-                  "one",
-                  "two",
-                  "three"
+                "name": "FormPage1",
+                "page": [
+                    {
+                        "type": "text",
+                        "label": "bar",
+                        "name": "text",
+                        "placeholder": "text"
+                    },
+                    {
+                        "type": "number",
+                        "label": "foo",
+                        "name": "number",
+                        "placeholder": "number",
+                        "initialValue": "0"
+                    },
+                    {
+                        "type": "divider",
+                        "name": "divider"
+                    },
+                    {
+                        "type": "radio",
+                        "label": "lorem ipsum sit dolor amet",
+                        "name": "radio",
+                        "options": [
+                          "one",
+                          "two",
+                          "three"
+                        ]
+                    }
                 ]
             },
             {
-                "type": "label",
-                "label": "hello",
-                "name": "hello"
-            },
-            {
-                "type": "checkbox",
-                "label": "sldkfhesoi;rhj",
-                "name": "check"
-            },
-            {
-                "type": "checkbox",
-                "label": "sldkfhesdsoi;rhj",
-                "name": "check2"
-            },
-            {
-                "type": "checkbox",
-                "label": ";lkdfn",
-                "name": "check3"
-            },
-            {
-                "type": "space",
-                "name": "sdlk"
-            },
-            {
-                "type": "number",
-                "name": "abcde",
-                "initialValue": "0",
-                "label": "Another number"
+                "name": "FormPage2",
+                "page": [
+                    {
+                        "type": "label",
+                        "label": "hello",
+                        "name": "hello"
+                    },
+                    {
+                        "type": "checkbox",
+                        "label": "sldkfhesoi;rhj",
+                        "name": "check"
+                    },
+                    {
+                        "type": "checkbox",
+                        "label": "sldkfhesdsoi;rhj",
+                        "name": "check2"
+                    },
+                    {
+                        "type": "checkbox",
+                        "label": ";lkdfn",
+                        "name": "check3"
+                    },
+                    {
+                        "type": "space",
+                        "name": "sdlk"
+                    },
+                    {
+                        "type": "number",
+                        "name": "abcde",
+                        "initialValue": "0",
+                        "label": "Another number"
+                    }
+                ]
             }
         ]
     }
 """.trimIndent()
 
-private fun getForm(): List<FormElement> {
+private fun getForm(): List<FormPage> {
     return Json.decodeFromString<Form>(formJSON.trimIndent()).form
 }
