a106e3637dc991487cbc7e8a52d9c0172377841d
liujip0 <136777216+liujip0@users.noreply.github.com>
2023-12-30 12:32:38 -0500

    Made labels of radio buttons and checkboxes clickable

--- app/app/src/main/java/com/example/isaproject/SummaryScreen.kt
+++ app/app/src/main/java/com/example/isaproject/SummaryScreen.kt
@@ -1,15 +1,10 @@
 package com.example.isaproject
 
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.OutlinedButton
-import androidx.compose.material3.Surface
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.stringResource
@@ -20,36 +15,27 @@ fun SummaryScreen(
     onSubmitButtonClicked: () -> Unit,
     modifier: Modifier = Modifier
 ) {
-    Column() {
+    Scaffold(
+        topBar = {
+            PageTitle(text = AppScreens.Summary.label)
+        },
+        bottomBar = {
+            BottomNavBar(
+                canNavigateBack = true,
+                nextButtonLabel = stringResource(R.string.submit),
+                onNextButtonClicked = onSubmitButtonClicked,
+                previousButtonLabel = stringResource(R.string.previous),
+                onPreviousButtonClicked = onPreviousButtonClicked
+            )
+        },
+        modifier = modifier
+    ) { innerPadding ->
         Text(
             text = "Congrats. You finished the form.",
             style = MaterialTheme.typography.displayLarge,
             modifier = Modifier
-                .weight(1f)
-                .align(Alignment.CenterHorizontally)
+                .padding(innerPadding)
                 .padding(all = dimensionResource(R.dimen.margin))
         )
-        Surface(
-            color = MaterialTheme.colorScheme.surfaceContainer
-        ) {
-            Row {
-                OutlinedButton(
-                    onClick = onPreviousButtonClicked,
-                    modifier = Modifier
-                        .padding(all = dimensionResource(R.dimen.margin))
-                        .weight(1f)
-                ) {
-                    Text(stringResource(R.string.previous))
-                }
-                Button(
-                    onClick = onSubmitButtonClicked,
-                    modifier = Modifier
-                        .padding(all = dimensionResource(R.dimen.margin))
-                        .weight(1f)
-                ) {
-                    Text(stringResource(R.string.submit))
-                }
-            }
-        }
     }
 }
