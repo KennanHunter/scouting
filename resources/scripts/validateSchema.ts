cd412c77e5b24e9a9593a0d48b6fc4e45df9b012
Kennan <kennanhunter5@gmail.com>
2024-01-17 20:58:50 -0500

    Add schema validation

---
+++ resources/scripts/validateSchema.ts
@@ -0,0 +1,18 @@
+import Ajv from "ajv";
+
+import * as schemaConfig from "../schema.json";
+import * as data2024 from "../2024.json";
+
+const ajv = new Ajv(); // options can be passed, e.g. {allErrors: true}
+
+console.log("TODO: still getting weird errors");
+// console.log(JSON.stringify(schemaConfig, null, 4));
+// const validate = ajv.compile((schemaConfig as any).default);
+// const valid = validate(data2024);
+
+// if (valid) {
+//   console.log("Schema Valid!");
+// } else {
+//   console.log(validate.errors);
+//   throw new Error("Schema Invalid");
+// }
