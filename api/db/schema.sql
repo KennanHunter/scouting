f43e96d07a3de5cef9cf2943633af85284c6abf8
Kennan <kennanhunter5@gmail.com>
2024-01-21 01:01:19 -0500

    i think this sql schema is good enough?

--- api/db/schema.sql
+++ api/db/schema.sql
@@ -1,6 +1,36 @@
 DROP TABLE IF EXISTS Teams;
-CREATE TABLE IF NOT EXISTS Teams (teamNumber TEXT PRIMARY KEY, teamNumber INTEGER KEY, nickname TEXT);
+
+CREATE TABLE IF NOT EXISTS Teams (
+  teamName TEXT,
+  teamNumber INTEGER UNIQUE PRIMARY KEY,
+  nickname TEXT
+);
 
 DROP TABLE IF EXISTS Events;
-CREATE TABLE IF NOT EXISTS Events (eventKey TEXT PRIMARY KEY, eventName TEXT, startTime INTEGER);
 
+CREATE TABLE IF NOT EXISTS Events (
+  eventKey TEXT UNIQUE PRIMARY KEY,
+  eventName TEXT NOT NULL,
+  startTime INTEGER NOT NULL
+);
+
+DROP TABLE IF EXISTS Match;
+
+CREATE TABLE IF NOT EXISTS Match (
+  matchKey TEXT UNIQUE PRIMARY KEY,
+  eventName TEXT,
+  startTime INTEGER,
+  eventKey TEXT,
+  FOREIGN KEY(eventKey) REFERENCES Events(eventKey)
+);
+
+DROP TABLE IF EXISTS TeamMatchEntry;
+
+CREATE TABLE IF NOT EXISTS TeamMatchEntry (
+  matchKey TEXT,
+  teamNumber INTEGER,
+  matchData TEXT,
+  PRIMARY KEY (matchKey, teamNumber),
+  FOREIGN KEY(matchKey) REFERENCES Match(matchKey),
+  FOREIGN KEY(teamNumber) REFERENCES Teams(teamNumber)
+);
