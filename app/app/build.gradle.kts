0bc39913419273376af3fa7495dc68c80404abd6
liujip0 <136777216+liujip0@users.noreply.github.com>
2024-02-17 17:50:48 -0500

    Generated 0.2.0 APK for testing on actual device

--- app/app/build.gradle.kts
+++ app/app/build.gradle.kts
@@ -14,7 +14,7 @@ android {
         minSdk = 26
         targetSdk = 34
         versionCode = 3
-        versionName = "0.1.2"
+        versionName = "0.2.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
