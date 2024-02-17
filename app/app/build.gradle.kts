62e85408ad4a4049c549b28bc4ac63b575839ab8
liujip0 <136777216+liujip0@users.noreply.github.com>
2024-02-17 18:04:21 -0500

    apk for 0.2.1

--- app/app/build.gradle.kts
+++ app/app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.example.isaproject"
         minSdk = 26
         targetSdk = 34
-        versionCode = 3
-        versionName = "0.2.0"
+        versionCode = 4
+        versionName = "0.2.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
