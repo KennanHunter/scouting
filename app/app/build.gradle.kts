315b017890c7060a469a8a3e71a67bd2fe72b473
liujip0 <136777216+liujip0@users.noreply.github.com>
2024-02-22 15:11:41 -0500

    Generated APK for 0.4.0

--- app/app/build.gradle.kts
+++ app/app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.example.isaproject"
         minSdk = 26
         targetSdk = 34
-        versionCode = 9
-        versionName = "0.3.4"
+        versionCode = 10
+        versionName = "0.4.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
