4435766a624fd4476742f98a9d3da870634affd3
liujip0 <136777216+liujip0@users.noreply.github.com>
2024-02-26 15:19:14 -0500

    Generated APK cause I forgot to do that last commit

--- app/app/build.gradle.kts
+++ app/app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "com.example.isaproject"
         minSdk = 26
         targetSdk = 34
-        versionCode = 17
-        versionName = "0.5.2"
+        versionCode = 18
+        versionName = "0.5.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
--- app/app/src/main/java/com/example/isaproject/DataSource.kt
+++ app/app/src/main/java/com/example/isaproject/DataSource.kt
@@ -278,12 +278,12 @@ object DataSource {
                                             "label": "First"
                                         },
                                         {
-                                            "value": "2",
-                                            "label": "Second"
-                                        },
-                                        {
                                             "value": "3",
                                             "label": "Third"
+                                        },
+                                        {
+                                            "value": "2",
+                                            "label": "Second"
                                         },
                                         {
                                             "value": "4",
