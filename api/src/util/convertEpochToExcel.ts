bc3e7f243f014d129ed8e141af6621265be83361
Kennan <kennanhunter5@gmail.com>
2024-03-02 14:42:45 -0500

    Fix timestamp generation

--- api/src/util/convertEpochToExcel.ts
+++ api/src/util/convertEpochToExcel.ts
@@ -2,7 +2,7 @@ export const convertEpochToExcel: (startTime: number) => string = (
   startTime
 ) => {
   // TODO: Fix this
-  const time = new Date();
+  const time = new Date(startTime * 1000);
 
   return `${time.getMonth()}/${time.getDay()}/${time.getFullYear()} ${time.toTimeString().split(" ")[0]}`;
 };
