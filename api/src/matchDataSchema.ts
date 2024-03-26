becb01ba38fb5a0847cab0c0df8a63ba50eddc7a
Kennan <kennanhunter5@gmail.com>
2024-03-26 13:57:00 -0400

    make passed notes optional

--- api/src/matchDataSchema.ts
+++ api/src/matchDataSchema.ts
@@ -25,7 +25,7 @@ export const matchDataSchema = z.object({
   onstageorder: z.string().nullable(),
   playeddefense: z.boolean().nullable(),
   position: z.string().nullable(),
-  passednotes: z.number().nullable(),
+  passednotes: z.number().nullable().optional(),
   rating: z.string().nullable(),
   receiveddefense: z.boolean().nullable(),
   scoutname: z.string(),
