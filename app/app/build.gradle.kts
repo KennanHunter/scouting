67c220c9b6e9563991535c42e204aa0336e45370
liujip0 <136777216+liujip0@users.noreply.github.com>
2024-04-05 00:18:45 -0400

    Update scout list for 2024incmp
    - Update dependencies
    
    0.9.5

--- app/app/build.gradle.kts
+++ app/app/build.gradle.kts
@@ -3,7 +3,7 @@ val ktor_version: String by project
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
-    id("org.jetbrains.kotlin.plugin.serialization") version "2.0.0-Beta2"
+    id("org.jetbrains.kotlin.plugin.serialization") version "2.0.0-Beta5"
     id("org.jetbrains.kotlin.plugin.parcelize")
 }
 
@@ -15,8 +15,8 @@ android {
         applicationId = "com.example.isaproject"
         minSdk = 30
         targetSdk = 34
-        versionCode = 40
-        versionName = "0.9.4"
+        versionCode = 41
+        versionName = "0.9.5"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
@@ -57,10 +57,10 @@ dependencies {
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
     implementation("androidx.activity:activity-compose:1.8.2")
-    implementation(platform("androidx.compose:compose-bom:2024.03.00"))
-    implementation("androidx.compose.ui:ui:1.6.4")
-    implementation("androidx.compose.ui:ui-graphics:1.6.4")
-    implementation("androidx.compose.ui:ui-tooling-preview:1.6.4")
+    implementation(platform("androidx.compose:compose-bom:2024.04.00"))
+    implementation("androidx.compose.ui:ui:1.6.5")
+    implementation("androidx.compose.ui:ui-graphics:1.6.5")
+    implementation("androidx.compose.ui:ui-tooling-preview:1.6.5")
     implementation("androidx.compose.material3:material3:1.2.1")
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.6.3")
     implementation("com.android.support:support-annotations:28.0.0")
@@ -68,17 +68,17 @@ dependencies {
     implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.7.0")
     implementation("androidx.navigation:navigation-runtime-ktx:2.7.7")
     implementation("androidx.navigation:navigation-compose:2.7.7")
-    implementation("androidx.compose.material:material-icons-extended:1.6.4")
+    implementation("androidx.compose.material:material-icons-extended:1.6.5")
     implementation("com.google.zxing:core:3.5.3")
     implementation("com.google.code.gson:gson:2.10.1")
     implementation("javax.inject:javax.inject:1")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-    androidTestImplementation(platform("androidx.compose:compose-bom:2024.03.00"))
-    androidTestImplementation("androidx.compose.ui:ui-test-junit4:1.6.4")
-    debugImplementation("androidx.compose.ui:ui-tooling:1.6.4")
-    debugImplementation("androidx.compose.ui:ui-test-manifest:1.6.4")
+    androidTestImplementation(platform("androidx.compose:compose-bom:2024.04.00"))
+    androidTestImplementation("androidx.compose.ui:ui-test-junit4:1.6.5")
+    debugImplementation("androidx.compose.ui:ui-tooling:1.6.5")
+    debugImplementation("androidx.compose.ui:ui-test-manifest:1.6.5")
     implementation("org.jetbrains.kotlin:kotlin-serialization-compiler-plugin-embeddable:2.0.0-Beta5")
     implementation("io.ktor:ktor-client-core:2.3.9")
     implementation("io.ktor:ktor-client-cio:2.3.9")
